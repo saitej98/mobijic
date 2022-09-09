d59094f45ae560746357b4187e9fae6d4a11caa4
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-09-09 11:40:59 +0530

    UI created

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Homepage from "./Components/homepage/homepage";
+import Login from "./Components/login/login";
+import Register from "./Components/register/register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+
+function App() {
+  const [user, setLoginUser] = useState({});
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user && user._id ? (
+              <Homepage setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
